99d44c99d3748f0e5acdff5aae4e9e81c3253914
Dallas Pataska <pataskad@gmail.com>
2021-11-30 23:24:32 -0600

    updated module.exports to properly output console message with function commented out to prevent errors

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-import "./styles.css";
-
-export function dropDownNav () {
+module.exports = function dropDownNav () {
   console.log("This is a message from the demo package");
-  const options = document.querySelectorAll(".navbar-option-main");
-  const dropDowns = document.querySelectorAll(".dropdown-menu");
-  window.addEventListener("click", function (e) {
-    if (e.target.matches(".navbar-option-main")) {
-      for (let i = 0; i < options.length; i++) {
-        if (options[i] === e.target) {
-          if (dropDowns[i].closest(".hidden")) {
-            dropDowns[i].classList.remove("hidden");
-          } else {
-            dropDowns[i].classList.add("hidden");
-          }
-        }
-      }
-    }
-  });
 }
+/* import "./styles.css";
 
 const options = document.querySelectorAll(".navbar-option-main");
 const dropDowns = document.querySelectorAll(".dropdown-menu");
@@ -34,3 +18,4 @@ window.addEventListener("click", function (e) {
     }
   }
 });
+ */
