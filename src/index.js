32df561e8d88a9b05d6dba4b04d4a58de3fecc23
Dallas Pataska <pataskad@gmail.com>
2021-11-30 22:54:12 -0600

    on click dropdown functionality added and working with minimal UI, scales so long as correct class names are used

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ import "./styles.css";
 export function dropDownNav () {
   console.log("This is a message from the demo package");
 }
+
+const options = document.querySelectorAll(".navbar-option-main");
+const dropDowns = document.querySelectorAll(".dropdown-menu");
+window.addEventListener("click", function (e) {
+  if (e.target.matches(".navbar-option-main")) {
+    for (let i = 0; i < options.length; i++) {
+      if (options[i] === e.target) {
+        if (dropDowns[i].closest(".hidden")) {
+          dropDowns[i].classList.remove("hidden");
+        } else {
+          dropDowns[i].classList.add("hidden");
+        }
+      }
+    }
+  }
+});
