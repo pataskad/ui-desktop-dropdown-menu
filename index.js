8b66f96983ffdd7d1bb5e3032f84d491f9555b9d
Dallas Pataska <pataskad@gmail.com>
2021-12-02 14:55:52 -0600

    removed commented out code

--- index.js
+++ index.js
@@ -28,28 +28,6 @@ const dropDownHover = (function navHover () {
     }
   });
 })();
-/* const options = document.querySelectorAll(".navbar-option-main");
-const dropDowns = document.querySelectorAll(".dropdown-menu");
-window.addEventListener("mouseover", function (e) {
-  if (e.target.matches(".navbar-option-main")) {
-    for (let i = 0; i < options.length; i++) {
-      if (options[i] === e.target) {
-        if (dropDowns[i].closest(".hidden")) {
-          dropDowns[i].classList.remove("hidden");
-        } else {
-          dropDowns[i].classList.add("hidden");
-        }
-      }
-    }
-  }
-});
-window.addEventListener("mouseover", function (e) {
-  if (e.target.matches("html") || e.target.matches("nav")) {
-    for (let i = 0; i < options.length; i++) {
-      dropDowns[i].classList.add("hidden");
-    }
-  }
-}); */
 
 module.exports.dropDownNav = function dropDownNav () {
   console.log("This is a test package, use at your own discretion");
