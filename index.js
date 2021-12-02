1fe4b2d2578f7fb4423e156d320c3269f2667afc
Dallas Pataska <pataskad@gmail.com>
2021-12-02 14:53:49 -0600

    Defined log messages for module to clarify if being ran outside or within browser environment, wrapped listener logic in IIFE to work in test environment and assigned to const

--- index.js
+++ index.js
@@ -4,8 +4,31 @@
  with the correct classNames to ensure js code picks up correct tags
  -- see html file for sample of how this works
 */
-
-const options = document.querySelectorAll(".navbar-option-main");
+const dropDownHover = (function navHover () {
+  const options = document.querySelectorAll(".navbar-option-main");
+  const dropDowns = document.querySelectorAll(".dropdown-menu");
+  window.addEventListener("mouseover", function (e) {
+    if (e.target.matches(".navbar-option-main")) {
+      for (let i = 0; i < options.length; i++) {
+        if (options[i] === e.target) {
+          if (dropDowns[i].closest(".hidden")) {
+            dropDowns[i].classList.remove("hidden");
+          } else {
+            dropDowns[i].classList.add("hidden");
+          }
+        }
+      }
+    }
+  });
+  window.addEventListener("mouseover", function (e) {
+    if (e.target.matches("html") || e.target.matches("nav")) {
+      for (let i = 0; i < options.length; i++) {
+        dropDowns[i].classList.add("hidden");
+      }
+    }
+  });
+})();
+/* const options = document.querySelectorAll(".navbar-option-main");
 const dropDowns = document.querySelectorAll(".dropdown-menu");
 window.addEventListener("mouseover", function (e) {
   if (e.target.matches(".navbar-option-main")) {
@@ -26,8 +49,14 @@ window.addEventListener("mouseover", function (e) {
       dropDowns[i].classList.add("hidden");
     }
   }
-});
+}); */
 
 module.exports.dropDownNav = function dropDownNav () {
   console.log("This is a test package, use at your own discretion");
+  if (typeof document === "undefined") {
+    return console.log("This is a server evaluation, DOM listeners will not run!");
+  } else {
+    console.log("This is a browser evaluation");
+    dropDownHover();
+  }
 };
